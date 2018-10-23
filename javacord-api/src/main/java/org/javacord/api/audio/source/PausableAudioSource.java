a2cc2ce37663910f9f7a763b7fbe237caeef1c58
Saladoc <saladoc42@gmail.com>
2018-10-23 11:38:14 +0200

    PausableAudioSource javadoc + isPaused method

--- javacord-api/src/main/java/org/javacord/api/audio/source/PausableAudioSource.java
+++ javacord-api/src/main/java/org/javacord/api/audio/source/PausableAudioSource.java
@@ -1,6 +1,16 @@
 package org.javacord.api.audio.source;
 
+/**
+ * An audio source that can be paused and resumed without dropping any frames in between.
+ */
 public interface PausableAudioSource extends AudioSource {
+
+    /**
+     * Check whether the audio source is paused.
+     *
+     * @return Whether the audio source is paused.
+     */
+    boolean isPaused();
 
     /**
      * Pauses the audio source (Makes hasNextFrame return false).
