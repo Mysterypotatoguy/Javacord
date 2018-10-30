663037fb3c7ab35abc4524436da21c51e6788c42
Mysterypotatoguy <mysterypotatoguy9@gmail.com>
2018-10-30 18:12:11 +0000

    Catch up to 3.0.1 with DispatchQueueSelector

--- javacord-core/src/main/java/org/javacord/core/util/handler/guild/voice/VoiceServerUpdateHandler.java
+++ javacord-core/src/main/java/org/javacord/core/util/handler/guild/voice/VoiceServerUpdateHandler.java
@@ -5,6 +5,7 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.event.server.voice.VoiceServerUpdateEvent;
 import org.javacord.api.listener.server.voice.VoiceServerUpdateListener;
+import org.javacord.core.entity.server.ServerImpl;
 import org.javacord.core.event.server.voice.VoiceServerUpdateEventImpl;
 import org.javacord.core.util.gateway.PacketHandler;
 
@@ -27,15 +28,17 @@ public class VoiceServerUpdateHandler extends PacketHandler {
 
     @Override
     public void handle(JsonNode packet) {
-        api.getServerById(packet.get("guild_id").asLong()).ifPresent(server -> {
-            String token = packet.get("token").asText();
-            String endpoint = packet.get("endpoint").asText();
-            VoiceServerUpdateEvent event = new VoiceServerUpdateEventImpl(server, token, endpoint);
-            List<VoiceServerUpdateListener> listeners = new ArrayList<>();
-            listeners.addAll(api.getObjectListeners(Server.class, server.getId(), VoiceServerUpdateListener.class));
-            listeners.addAll(api.getListeners(VoiceServerUpdateListener.class));
+        api.getServerById(packet.get("guild_id").asLong())
+                .map(server -> (ServerImpl) server)
+                .ifPresent(server -> {
+                    String token = packet.get("token").asText();
+                    String endpoint = packet.get("endpoint").asText();
+                    VoiceServerUpdateEvent event = new VoiceServerUpdateEventImpl(server, token, endpoint);
+                    List<VoiceServerUpdateListener> listeners = new ArrayList<>();
+                    listeners.addAll(api.getObjectListeners(Server.class, server.getId(), VoiceServerUpdateListener.class));
+                    listeners.addAll(api.getListeners(VoiceServerUpdateListener.class));
 
-            api.getEventDispatcher().dispatchVoiceServerUpdateEvent(server, server, event);
+                    api.getEventDispatcher().dispatchVoiceServerUpdateEvent(server, server, event);
         });
     }
 }
